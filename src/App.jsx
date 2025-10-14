f8fbf3a698565c117755a97aa795d058595b4545
nahuel echeto <echetonahuel@gmail.com>
2025-10-13 22:34:08 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar"
+import ItemListContainer from "./components/ItemListContainer"
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar/>
+      <ItemListContainer saludo='Bienvenido a Big Burger'/>
+    </>
+  )
+}
+
+export default App
